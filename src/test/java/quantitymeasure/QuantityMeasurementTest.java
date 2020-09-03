5df7f6d9a27a29f966e7168f14ffc7cd6eba2da2
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-03 15:45:32 +0530

    Added Code and Test for Zero feet and Zero Feet when compared should return Equal

---
+++ src/test/java/quantitymeasure/QuantityMeasurementTest.java
@@ -0,0 +1,17 @@
+package quantitymeasure;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class QuantityMeasurementTest {
+    
+    @Test
+    public void givenZeroFeetAndZeroFeet_WhenLengthsCompared_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+}

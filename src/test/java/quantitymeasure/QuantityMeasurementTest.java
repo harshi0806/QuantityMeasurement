33b76e294863469750615cf4737a98fb35743249
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-04 02:00:39 +0530

    Added Test case to check Zero Inch and One Inch when compared should return Not Equals

--- src/test/java/quantitymeasure/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasure/QuantityMeasurementTest.java
@@ -75,4 +75,11 @@ public class QuantityMeasurementTest {
         Inch inch2 = new Inch(0.0);
         Assert.assertEquals(inch1, inch2);
     }
+
+    @Test
+    public void givenZeroInchAndOneInch_WhenComparedForEquality_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1, inch2);
+    }
 }

3f3fe838664b7e5d5333a2fd6218fd1c31751d64
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-03 16:08:55 +0530

    Added Test case to check Zero Feet and One Feet when compared should return Not Equals

--- src/test/java/quantitymeasure/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasure/QuantityMeasurementTest.java
@@ -14,4 +14,11 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void givenZeroFeetAndOneFeet_WhenLengthsCompared_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
 }

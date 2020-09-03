4ca92cad3b23dea546e4bf456d6a3c8f21838946
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-04 01:48:31 +0530

    Modified Test method name when Compared for Equality

--- src/test/java/quantitymeasure/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasure/QuantityMeasurementTest.java
@@ -11,14 +11,14 @@ import java.util.Objects;
 public class QuantityMeasurementTest {
     
     @Test
-    public void givenZeroFeetAndZeroFeet_WhenLengthsCompared_ShouldReturnEqual() {
+    public void givenZeroFeetAndZeroFeet_WhenComparedForEquality_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
-    public void givenZeroFeetAndOneFeet_WhenLengthsCompared_ShouldReturnNotEqual() {
+    public void givenZeroFeetAndOneFeet_WhenComparedForEquality_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
         Assert.assertNotEquals(feet1, feet2);
